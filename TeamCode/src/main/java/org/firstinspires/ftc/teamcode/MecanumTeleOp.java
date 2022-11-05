fa56214d7c099704647daf4ccaa2e51facc75a70
jefft <rejatkrishnan@gamil.com>
2022-11-05 13:55:36 -0500

    use this version 115

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -15,29 +15,40 @@ public class MecanumTeleOp extends LinearOpMode {
         DcMotor backLeft = hardwareMap.dcMotor.get("bl");
         DcMotor frontRight = hardwareMap.dcMotor.get("fr");
         DcMotor backRight = hardwareMap.dcMotor.get("br");
+        CRServo spin1 = hardwareMap.get(CRServo.class, "spin1");
+        CRServo spin2 = hardwareMap.get(CRServo.class, "spin2");
 
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double vertical = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double horizontal = gamepad1.left_stick_x; // Counteract imperfect strafing
+            double vertical = -gamepad1.left_stick_x; // Remember, this is reversed!
+            double horizontal = gamepad1.left_stick_y; // Counteract imperfect strafing
             double pivot = gamepad1.right_stick_x;
 
-            double frontLeftPower = (pivot + vertical + horizontal);
-            double backLeftPower = (pivot + vertical - horizontal);
+            double frontLeftPower = (pivot - vertical + horizontal);
+            double backLeftPower = (pivot - vertical - horizontal);
             double frontRightPower = (-pivot + vertical - horizontal);
-            double backRightPower = (-pivot + vertical + horizontal);
+            double backRightPower = (pivot + vertical - horizontal);
 
             frontLeft.setPower(frontLeftPower/2.5);
             backLeft.setPower(backLeftPower/2.5);
             frontRight.setPower(frontRightPower/2.5);
             backRight.setPower(backRightPower/2.5);
+
+            if (gamepad1.right_trigger > 0.1) {
+                spin1.setPower(gamepad1.right_trigger);
+                spin2.setPower(-gamepad1.right_trigger);
+            } else if (gamepad1.left_trigger > 0.1) {
+                spin1.setPower(-gamepad1.left_trigger);
+                spin2.setPower(gamepad1.left_trigger);
+            } else {
+                spin1.setPower(0);
+                spin1.setPower(0);
+            }
         }
     }
 }
